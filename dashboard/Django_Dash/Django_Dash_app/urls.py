df5cf38e6577e6cdd401f0ad7916e5769897ffc1
shufan xia <sxia1@haverford.edu>
2019-04-15 23:59:35 -0400

    Create Django project

---
+++ dashboard/Django_Dash/Django_Dash_app/urls.py
@@ -0,0 +1,33 @@
+"""Django_Dash URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+from . import views
+from django.views.generic import TemplateView
+
+# Load plotly apps - this triggers their registration
+import Django_Dash_app.dashboard.dashboard_app
+
+
+app_name="Django_Dash_app"
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index/', views.index, name='index'),
+    path('django_plotly_dash/', include('django_plotly_dash.urls')),
+   # path('dashboard',dashboard, name="dashboard")
+]
