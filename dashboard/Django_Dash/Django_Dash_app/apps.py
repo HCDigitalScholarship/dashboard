c62aecdb6f0e0af362c635ac43ba4873e1996035
Yongxin Xu <yongxinxu@Yongxins-MacBook-Air.local>
2019-04-16 13:37:48 -0400

    remove duplicate files

--- dashboard/Django_Dash/Django_Dash_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoDashAppConfig(AppConfig):
-    name = 'Django_Dash_app'
-
--- dashboard/Django_Dash/Django_Dash_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-import Django_Dash_app.dashboard.dashboard_app
-
-# Create your views here.
-def index(request):
-    return render(request, "index.html")
